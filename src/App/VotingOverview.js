2e26f97a7167bf5ab8839ad3d506378ea8408ad5
siuming <zephyrwong.private@gmail.com>
2017-06-26 17:30:04 +0800

    Add VotingOverview

---
+++ src/App/VotingOverview.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Reflux from 'reflux';
+import PollingResultChart from './PollingResultChart';
+import Toolbar from './Toolbar';
+
+class VotingOverview extends Reflux.Component {
+  render() {
+    return (
+      <div>
+        <Toolbar />
+        
+      </div>
+    );
+  }
+}
+
+export default VotingOverview;
